9312afbfee27d3e1d36a9f80222df742f4bbaafc
Maulidin <mmaulidinabdi@mail.com>
2025-04-27 09:13:58 +0800

    add circleCI config

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# build image
+docker build -t ghcr.io/mmaulidinabdi/karsajobs:latest .
+
+
+#login ke github packages
+echo $GITHUB_TOKEN | docker login ghcr.io -u mmaulidinabdi --password-stdin
+
+#push image ke github packages
+docker push ghcr.io/mmaulidinabdi/karsajobs:latest
