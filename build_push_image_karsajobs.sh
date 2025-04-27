283cfd367182c2352d633f15faf6eea446aae152
Maulidin <mmaulidinabdi@mail.com>
2025-04-27 18:55:09 +0800

    komentar

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# build image
+# build image di direktori saat ini
 docker build -t ghcr.io/mmaulidinabdi/karsajobs:latest .
-
 
 #login ke github packages
 echo $GITHUB_TOKEN | docker login ghcr.io -u mmaulidinabdi --password-stdin
